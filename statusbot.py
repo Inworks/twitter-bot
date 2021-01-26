0abd416299b6c4d4a1f06fc77b6f9a20049434b4
Daniel Rodriguez <Omni_portable@Omni-portable.local>
2021-01-26 13:28:13 -0700

    test commit

---
+++ statusbot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from functions import *
+import tweepy
+import logging
+from config import create_api #this takes the credentials exported to the venv
+import time
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+int statcounter=0;
+
+def main():
+    
+    testdict = {}
+    tags = []
+    statusString = "filler text"
+    tempString = "temp"
+    projectImport(testdict)
+    statusString = titleTags(testdict,tags)
+    
+    api = create_api()
+    api.update_status(statusString)
+    while True:
+        logger.info("Waiting...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
